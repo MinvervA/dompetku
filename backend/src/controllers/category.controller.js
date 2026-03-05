4d8228a913ab02100d56a7a1bc82f53798b26831
andrean tan <andrean923@gmail.com>
2026-03-06 04:43:55 +0700

    feat(categories): add get categories endpoint

---
+++ backend/src/controllers/category.controller.js
@@ -0,0 +1,21 @@
+import prisma from "../lib/prisma.js";
+
+export const getCategories = async (req, res) => {
+  try {
+    const categories = await prisma.category.findMany({
+      orderBy: { name: "asc" },
+    });
+
+    // statusnya harus 200 karena bersifat mengambil bukan create ( 201)
+    return res.status(200).json({
+      success: true,
+      data: categories,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error!",
+    });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { authRoute } from "./routes/auth.route.js";
+import { categoriesRoute } from "./routes/category.route.js";
 
 // load env data
 dotenv.config();
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoute);
+app.use("/api/categories", categoriesRoute);
 
 const PORT = process.env.PORT || 5000;
 

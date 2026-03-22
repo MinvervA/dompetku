10430a5b6d55826b5883b26b6d8c00284992a651
MinvervA <andrean923@gmail.com>
2026-03-22 23:52:42 +0700

    feat(transaction): added transaction card 1/2 progress

--- frontend/src/components/dashboard/TransactionCard.jsx
+++ frontend/src/components/dashboard/TransactionCard.jsx
@@ -1,8 +1,30 @@
+import {
+  Briefcase,
+  Car,
+  Gamepad2,
+  Heart,
+  MoreHorizontal,
+  ShoppingBag,
+  UtensilsCrossed,
+  Wrench,
+  Zap,
+} from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 export const TransactionCard = ({ transaction, isLoading }) => {
   console.log(transaction);
   console.log(isLoading);
+  const iconMap = {
+    food: UtensilsCrossed,
+    transport: Car,
+    bills: Zap,
+    shopping: ShoppingBag,
+    salary: Briefcase,
+    health: Heart,
+    entertainment: Gamepad2,
+    freelance: Wrench,
+    others: MoreHorizontal,
+  };
 
   return (
     <Card>
@@ -17,14 +39,26 @@ export const TransactionCard = ({ transaction, isLoading }) => {
             <transaction.category.icon />
           </div>
         </div> */}
-        {transaction.map((item, index) => {
-          // const icon = iconMap[]
+        {transaction?.map((item, index) => {
+          const IconComponent = iconMap[item.category.icon] ?? MoreHorizontal;
+          const isIncome = item.type === "INCOME";
+          console.log(item.category.icon);
+
           return (
             <div className="" key={index}>
-              <div
-                className={`p-2 ${item.type === "EXPENSE" ? "bg-red-900" : "bg-green-900"}`}
-              >
-                {/* <item.description.category.icon /> */}
+              <div className={`mb-5 flex gap-2`}>
+                <div
+                  className={`${isIncome ? "text-green-600" : "text-red-600"} font-bold p-2 w-fit border-black border`}
+                >
+                  <IconComponent className={`w-6 h-6`} />
+                </div>
+                <div className="flex">
+                  <div className="flex items-center">
+                    <div className="">{item.description}</div>
+                    <div className=""></div>
+                  </div>
+                  <div className=""></div>
+                </div>
               </div>
             </div>
           );
